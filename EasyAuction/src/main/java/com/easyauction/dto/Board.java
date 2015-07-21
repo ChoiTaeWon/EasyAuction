7655347625ada8ad1e5dae78d660dea7e55dadb1
Administrator <Administrator@3C10>
2015-07-21 10:46:34 +0900

    11

---
+++ EasyAuction/src/main/java/com/easyauction/dto/Board.java
@@ -0,0 +1,104 @@
+package com.easyauction.dto;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
+
+public class Board implements Serializable{
+	
+	private int bdNo;
+	private int bdType;
+	private String bdContent;
+	private String bdWriter;
+	private String bdTitle;
+	private int bdReadCount;
+	private Date bdDate;
+	private int bdReportingCount;
+	private boolean bdBlindCheck;
+	
+	// 데이터베이스의 Board 와 BoardComment 테이블 간의 1:M 관계를 구현한 필드 
+	private List<BoardComment> comments;
+		
+	public Board() {}
+	
+	public List<BoardComment> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<BoardComment> comments) {
+		this.comments = comments;
+	}
+
+	public int getBdNo() {
+		return bdNo;
+	}
+
+	public void setBdNo(int bdNo) {
+		this.bdNo = bdNo;
+	}
+
+	public int getBdType() {
+		return bdType;
+	}
+
+	public void setBdType(int bdType) {
+		this.bdType = bdType;
+	}
+
+	public String getBdContent() {
+		return bdContent;
+	}
+
+	public void setBdContent(String bdContent) {
+		this.bdContent = bdContent;
+	}
+
+	public String getBdWriter() {
+		return bdWriter;
+	}
+
+	public void setBdWriter(String bdWriter) {
+		this.bdWriter = bdWriter;
+	}
+
+	public String getBdTitle() {
+		return bdTitle;
+	}
+
+	public void setBdTitle(String bdTitle) {
+		this.bdTitle = bdTitle;
+	}
+
+	public int getBdReadCount() {
+		return bdReadCount;
+	}
+
+	public void setBdReadCount(int bdReadCount) {
+		this.bdReadCount = bdReadCount;
+	}
+
+	public Date getBdDate() {
+		return bdDate;
+	}
+
+	public void setBdDate(Date bdDate) {
+		this.bdDate = bdDate;
+	}
+
+	public int getBdReportingCount() {
+		return bdReportingCount;
+	}
+
+	public void setBdReportingCount(int bdReportingCount) {
+		this.bdReportingCount = bdReportingCount;
+	}
+
+	public boolean isBdBlindCheck() {
+		return bdBlindCheck;
+	}
+
+	public void setBdBlindCheck(boolean bdBlindCheck) {
+		this.bdBlindCheck = bdBlindCheck;
+	}
+	
+}
