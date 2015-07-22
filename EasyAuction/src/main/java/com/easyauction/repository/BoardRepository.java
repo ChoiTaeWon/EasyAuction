983e261cc1a6e8447eb116bc24b174f7311c5032
choi <choi@choi-PC>
2015-07-22 16:18:06 +0900

    보드 빈, 마이바티스 등록

--- EasyAuction/src/main/java/com/easyauction/repository/BoardRepository.java
+++ EasyAuction/src/main/java/com/easyauction/repository/BoardRepository.java
@@ -1,5 +1,7 @@
 package com.easyauction.repository;
 
+import com.easyauction.dto.Board;
+
 public interface BoardRepository {
-	
+	public void insertPhoto(Board board);
 }
--- EasyAuction/src/main/java/com/easyauction/repository/OracleBoardRepository.java
+++ EasyAuction/src/main/java/com/easyauction/repository/OracleBoardRepository.java
@@ -1,10 +1,10 @@
 package com.easyauction.repository;
 
-import org.apache.poi.ss.formula.functions.Vlookup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
+import com.easyauction.dto.Board;
 import com.easyauction.mapper.BoardMapper;
 
 @Repository(value="boardRepository")
@@ -16,5 +16,10 @@ public class OracleBoardRepository implements BoardRepository{
 	@Qualifier("boardMapper")
 	public void setBoardMapper(BoardMapper boardMapper){
 		this.boardMapper = boardMapper;
+	}
+	
+	@Override
+	public void insertPhoto(Board board){
+		 
 	}
 }
