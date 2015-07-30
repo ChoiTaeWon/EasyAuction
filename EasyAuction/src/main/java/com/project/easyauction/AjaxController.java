a60da0c4b2442bbaba7ef5d0374e1ab0d432c9b7
hk <hk@hklee-home>
2015-07-30 21:59:01 +0900

    fdgsdfg

--- EasyAuction/src/main/java/com/project/easyauction/AjaxController.java
+++ EasyAuction/src/main/java/com/project/easyauction/AjaxController.java
@@ -14,6 +14,7 @@ import com.easyauction.common.Util;
 import com.easyauction.dto.AuctionReporting;
 import com.easyauction.dto.Bidding;
 import com.easyauction.dto.BoardReporting;
+import com.easyauction.dto.Member;
 import com.easyauction.dto.MemberReporting;
 import com.easyauction.service.AuctionService;
 import com.easyauction.service.BiddingService;
@@ -213,9 +214,10 @@ public class AjaxController {
 		memberReporting.setMrpTarget(targetmbId);
 		memberReporting.setMrpContent(reportText);
 		
-		System.out.println("신고하기 내용" + "////" + reporter +"////" +targetmbId +"////" +reportText );
 		reportService.insertMemberReportingBytargetmbId(memberReporting);
-		
+		Member member = new Member();
+		member.setMbId(targetmbId);
+		mbgsvc.setEditMember(member);
 		return result;
 	}
 		
